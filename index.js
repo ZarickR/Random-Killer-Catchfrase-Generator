f8f4d53dfc6f6a2bc4afb31754d56c3cd2b63c0f
Zack Riquelme <179593684+ZarickR@users.noreply.github.com>
2024-11-21 20:46:57 -0500

    Add arrays with the information for each killer, phrase, action and location.

---
+++ index.js
@@ -0,0 +1,39 @@
+//Arrays for the names of the Killers, Phrases, Actions and Locations
+
+const killers = ["Freddy Krueger", 
+                 "Jason Voorhees", 
+                 "Michael Myers", 
+                 "Ghostface", 
+                 "Chucky"];
+const phrases = ["Welcome to prime time!", 
+                 "Sweet dreams.", 
+                 "Every town has an Elm Street!", 
+                 "I am eternal.", 
+                 "This is God.", 
+                 "What's your favorite scary movie?", 
+                 "Do you like scary movies?", 
+                 "Hello, Sidney.", 
+                 "I want to play a game.", 
+                 "This is the last person you're ever going to see alive.", 
+                 "Hi, I'm Chucky. Wanna play?", 
+                 "Friends till the end!", 
+                 "It's your friend till the end!", 
+                 "You've been very naughty.", 
+                 "This is the end, friend!"];
+const actions = ["Heavy breathing", 
+                 "Footsteps in the woods", 
+                 "Breaking branches", 
+                 "Ki ki ki, ma ma ma", 
+                 "Head tilt", 
+                 "Slow methodical walking", 
+                 "Standing still while observing", 
+                 "Heavy breathing behind mask"];
+const locations = ["Elm Street", 
+                   "Dream World",
+                   "Boiler Room",
+                   "Camp Crystal Lake",
+                   "Haddonfield, Illinois",
+                   "Woodsboro, California",
+                   "Toy Store"];
+
+//
